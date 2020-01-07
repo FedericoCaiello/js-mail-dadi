67d1277b001ad45957f6c96f56805a57c77ee6d4
Federico <federico.caiello@gmail.com>
2020-01-07 19:23:41 +0100

    email completata js

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,15 @@
-var email = ['federico@gmail.com', 'marco@gmail.com','stefano@gmail.com', 'antonio@gmail.com'];
-var emailUtente = prompt('inserisci la tua email');
-for (var i = 0; i < email.length; i++) {
-  // console.log(email[i]);
+var email = ["federico@gmail.com","nino@gmail.com","stefano@gmail.com","antonio@gmail.com"];
+console.log(email[0])
+var sceltaUtente = prompt('inserisci la tua email');
+  console.log(sceltaUtente);
+var trovato = false;
+alert('scelta errata')
+for (var i = 0; i<email.length; i++){
   var emailEsatta = email[i];
-  if (emailEsatta == 'federico@gmail.com') {
-    console.log(emailEsatta);
+  if (sceltaUtente == emailEsatta) {
+    trovato = true;
+    alert('benvenuto')
+
   }
 }
+console.log(trovato);
